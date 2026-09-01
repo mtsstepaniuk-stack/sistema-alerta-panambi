9bc2d17e196fb2c1e74e019335ce0502549cd431
mtsstepaniuk-stack <mtsstepaniuk@gmail.com>
2026-09-01 19:22:54 -0300

    Activar diseño responsive móvil

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ import './rf14-pending-fix.js?v=20260901-3';
 import './rnf1.js';
 import './rnf2.js';
 import './map-fullwidth.js';
+import './mobile-layout.js';
 import { refreshUserMenu } from './auth.js';
 
 // Ajustes visuales puntuales del login.
